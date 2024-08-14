d6ba62d6e884e1e81ca487a9006e9d71c7762927
hdrees97 <hdrees70@googlemail.com>
2024-08-14 21:14:59 +0200

    Added testrp2040

---
+++ keyboards/testrp2040/keymaps/default/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define IS31FL3731_I2C_ADDRESS_1 IS31FL3731_I2C_ADDRESS_GND
+
+/* Encoder defines */
+#define ENCODERS_PAD_A { GP1 }
+#define ENCODERS_PAD_B { GP2 }
+
---
+++ keyboards/testrp2040/keymaps/default/keymap.c
@@ -0,0 +1,44 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┐
+     * │ A │
+     * └───┘
+     */
+    [0] = LAYOUT_ortho_1x1(
+        KC_A
+    )
+};
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (clockwise) {
+        tap_code(KC_A); // Lautstärke erhöhen
+    } else {
+        tap_code(KC_A); // Lautstärke verringern
+    }
+    return true;
+}
+
+/*
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    }
+    else if (index == 1) {
+        if (clockwise) {
+            tap_code(KC_PGDN);
+        } else {
+            tap_code(KC_PGUP);
+        }
+    }
+    return false;
+}
+*/
